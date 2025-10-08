391bdf951a6f1f95f318ac4ea7cdcaa0fbaa2a02
uditarawat30-tech <uditarawat30@gmail.com>
2025-10-08 14:42:46 +0530

    Update dashboard.py

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,1 +1,10 @@
+from attendance import attendance_log
+from leave import leave_requests
 
+def view_attendance(student_id=None):
+    if student_id:
+        return attendance_log.get(student_id, [])
+    return attendance_log
+
+def view_leaves():
+    return leave_requests
