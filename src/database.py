0b815d3fa19ca200155580a48cc43935bc5cbc8b
uditarawat30-tech <uditarawat30@gmail.com>
2025-11-16 15:39:10 +0530

    Delete src directory

--- src/database.py
+++
@@ -1,8 +0,0 @@
-students = {}
-
-def add_student(student_id, name, room_no):
-    students[student_id] = {'name': name, 'room': room_no}
-    return "Student added"
-
-def get_student(student_id):
-    return students.get(student_id, "Student not found")
